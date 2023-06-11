579b7c84bd697ab4dd448018402192465d550423
ALivanskoy <a.a.shutov@bk.ru>
2023-06-11 10:01:47 +0300

    Homework1

---
+++ Homework1/Main.java
@@ -0,0 +1,10 @@
+import FirstPackage.SomeClass;
+import SecondPackage.AnotherClass;
+
+public class Main {
+
+    SomeClass sc = new SomeClass(1, "First String", "SecondString");
+    AnotherClass ac = new AnotherClass(true, 84.2 );
+
+    
+}
